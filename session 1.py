5a4e30370dc2b7bea47efd6acdb2ee437977ca34
viniflachi <vcarvalho250406@gmail.com>
2026-01-19 16:14:32 +0100

    First commit ever

---
+++ session 1.py
@@ -0,0 +1,14 @@
+print("Hello World")
+print('Hello World')
+print(2+5)
+print("2+5")
+print("'And John asked how are you?")
+print('"'"")
+print("\"'")
+print("2+3+4+5=", 2+3+4+5)
+#you can print multiple things with comma separator
+print("my age is",22+22, "and I am", 100+86, "cm tall")
+#We can change how parameters are spaced out
+print("my age is",40)
+print(1,2,3,4,40, sep="bob", end="\n\n\n")
+print("my age is: ",22+22, ". I am: ", 100+86, "cm tall.",sep="")
